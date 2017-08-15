5548986a3c9141fc048b93db6bb37be11f73d0b0
Mallorax <mallorax@gmail.com>
2017-08-15 17:36:29 +0200

    Added Tabs to main activity and made static side menu

---
+++ MyApp/app/src/main/java/pl/patrykzygo/pocketleague/ui/StaticInfoFragment.java
@@ -0,0 +1,32 @@
+package pl.patrykzygo.pocketleague.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.patrykzygo.pocketleague.R;
+
+/**
+ * Created by Mallorax on 15.08.2017.
+ */
+
+public class StaticInfoFragment extends Fragment {
+
+    public StaticInfoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.static_info_fragment, container, false);
+    }
+}
